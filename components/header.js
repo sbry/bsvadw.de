931ea13db4840db85cf5ec8a3626355c8de3840d
Justin Salisbury <justin@salisbury.de>
2021-09-21 11:39:36 +0200

    cleaned up the menu

--- components/header.js
+++ components/header.js
@@ -20,25 +20,6 @@ export default function Header() {
                         {name: 'Kontakt', href: '/kontakt'},
                         {name: 'Sporthalle', href: '/sporthalle'},
                         {name: 'Geschichte', href: '/geschichte'},
-                    ].map((item) => (
-                        <ActiveLink
-
-                            key={item.name}
-                            href={item.href}
-                            className={item.current ? 'active' : ''}
-                            aria-current={item.current ? 'page' : undefined}
-                        >
-                            {item.name}
-                        </ActiveLink>
-                    ))}
-                    <a
-                        className={"external-link"}
-                        href={"https://bettv.tischtennislive.de/default.aspx?L1=Public&L2=Verein&L2P=1198&Page=Spielbetrieb&Sportart=96"}>
-                        BeTTV-Spielbetrieb
-                    </a>
-
-
-                    {[
                         {name: 'Impressum', href: '/impressum'},
                         {name: 'Datenschutz', href: '/datenschutz'},
                     ].map((item) => (
@@ -52,6 +33,7 @@ export default function Header() {
                             {item.name}
                         </ActiveLink>
                     ))}
+
                 </nav>
             </div>
         </header>
--- components/layout.js
+++ components/layout.js
@@ -10,18 +10,35 @@ export default function Layout({children}) {
                 <title>BSV AdW e.V. Tischtennis</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <div title={"Berliner Sportverein Akademie der Wissenschaften e.V."} className={'mx-5 font-bold text-white text-xl sm:text-2xl py-2'}>
-                <a href="/">
-                    <div className={" "}>
+            <div className={"grid grid-cols-1 sm:grid-cols-2 mb-5 sm:mb-1"}>
+                <div title={"Berliner Sportverein Akademie der Wissenschaften e.V."}
+                     className={'mx-5 font-bold text-white text-xl md:text-2xl py-2'}>
+                    <a href="/">
                         <img
-                            className={"inline-block w-10 h-10 mr-2"} src="/logo/adw-symbol.png"
+                            className={"inline-block w-12 h-12 mr-2"} src="/logo/adw-symbol.png"
                             alt="AdW"
                         />
                         BSV AdW e.V. Tischtennis
-                    </div>
-                </a>
+                    </a>
 
 
+                </div>
+                <div title={"Spielbetrieb des BSV AdW beim Berliner Tischtennisverband"}
+                     className={'mx-5 font-bold text-white text-xl md:text-2xl py-2'}
+                >
+                    <a
+                        className={""}
+                        href={"https://bettv.tischtennislive.de/default.aspx?L1=Public&L2=Verein&L2P=1198&Page=Spielbetrieb&Sportart=96"}>
+                        <img
+                            className={"inline-block w-12 mr-3"} src="/logo/tt-icon.svg"
+                            alt="AdW"
+                        />
+                        BeTTV-Spielbetrieb
+
+                    </a>
+
+
+                </div>
             </div>
             <Header/>
 
