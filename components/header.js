9c0cf84fe827197a534737b793b38286f2a8d8ca
Justin Salisbury <justin@salisbury.de>
2021-09-21 13:30:49 +0200

    use activelink

--- components/header.js
+++ components/header.js
@@ -1,34 +1,20 @@
-import Link from 'next/link'
-
-
 import ActiveLink from "./activelink";
-
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
 
 export default function Header() {
     return (
         <header className="">
-
             <div className="flex ml-5">
-
                 <nav className={"rounded-t-md"}>
-
                     {[
-                        {name: 'Kontakt', href: '/kontakt'},
-                        {name: 'Sporthalle', href: '/sporthalle'},
-                        {name: 'Geschichte', href: '/geschichte'},
-                        {name: 'Impressum', href: '/impressum'},
-                        {name: 'Datenschutz', href: '/datenschutz'},
+                        {name: 'Kontakt', href: '/kontakt/'},
+                        {name: 'Sporthalle', href: '/sporthalle/'},
+                        {name: 'Geschichte', href: '/geschichte/'},
+                        {name: 'Impressum', href: '/impressum/'},
+                        {name: 'Datenschutz', href: '/datenschutz/'},
                     ].map((item) => (
                         <ActiveLink
-
                             key={item.name}
                             href={item.href}
-                            className={item.current ? 'active' : ''}
-                            aria-current={item.current ? 'page' : undefined}
                         >
                             {item.name}
                         </ActiveLink>
