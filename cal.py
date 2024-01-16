f28b9829c76f2106a7f38e0030be0e62d4e136c0
Justin Salisbury <justin@salisbury.de>
2024-01-16 17:10:00 +0100

    getting the calendars

--- cal.py
+++ cal.py
@@ -6,20 +6,22 @@
 # Created:  Tue Jan 16 16:23:57 2024
 #
 
-class Test:
-    pass
-
-from ics import Calendar
-import requests
+import ics, requests, calendar
  
 # Parse the URL
-url = "https://bettv.tischtennislive.de/export/Tischtennis/iCal.aspx?Typ=Verein&ID=***REMOVED***&Runde=2&Hallenplan=True"
-cal = Calendar(requests.get(url).text)
+bettv_url = "https://bettv.tischtennislive.de/export/Tischtennis/iCal.aspx?Typ=Verein&ID=***REMOVED***&Runde=2&Hallenplan=True"
+bettv_cal = ics.Calendar(requests.get(bettv_url).text)
  
-# Print all the events
-print(cal.events)
+google_url = "https://calendar.google.com/calendar/ical/***REMOVED***%40group.calendar.google.com/public/basic.ics"
+google_cal = ics.Calendar(requests.get(google_url).text)
 
 if __name__ == '__main__':
+    print(google_cal.events)
+    
+    kalenderblatt = calendar.TextCalendar(calendar.MONDAY)
+    ausgabe = kalenderblatt.formatmonth(2020,1)
+    print(ausgabe)
+    
     pass
 
 
