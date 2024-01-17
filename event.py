5e49eda8e039617c5ef6131f3377bfeb22a91f7a
Justin Salisbury <technik@perlentaucher.de>
2024-01-17 19:06:36 +0100

    some really smart html

--- event.py
+++ event.py
@@ -8,47 +8,26 @@ import datetime
 import calendar
 from dateutil.relativedelta import relativedelta
 
-# from general.string.xmlout import XmlOut
-# from event.models import Event
-# from django.core import urlresolvers
+from lxml import etree
+from lxml.builder import E
+
+def prettyprint(element, **kwargs):
+    xml = etree.tostring(element, pretty_print=True, **kwargs)
+    print(xml.decode(), end='')
+
+def stringify(element):
+    return etree.tostring(element).decode()
+
+# th = E.th(E.a(href="test2"), test="1")
+# prettyprint(th)
+# print(stringify(th))
 
 
-import xml.etree.ElementTree as ET
-
-
-# the_doc = ET.Element("tag", testr="1")
-# b = ET.SubElement(the_doc, 'b')
-# b.text = "this element also contains text"
-# etreeString = ET.tostring(the_doc, encoding='unicode')
-# print(etreeString)
-
-def events():
-    return []
-
-
-def events_by_day(all_events):
-    events = {}
-    for event in all_events:
-        try:
-            events[event.date_start].append(event)
-        except KeyError:
-            events[event.date_start] = []
-            events[event.date_start].append(event)
-        pass
-    return events
-
-
-class EventCalendar:
-    """
-        fat dual-use calendar:
-            - sets lots of context
-            - renders as string
-    """
-
+class Calendar:
     def __init__(self, date_cursor=datetime.date.today()):
         self.date_cursor = date_cursor
         self.months = []
-        month = EventCalendarMonth(self, self.date_cursor)
+        month = CalendarMonth(self, self.date_cursor)
         self.months.append(month)
 
     def __str__(self):
@@ -74,7 +53,7 @@ class EventCalendar:
         return args
 
 
-class EventCalendarMonthNavigation:
+class CalendarMonthNavigation:
 
     def __init__(self, date_cursor, delta=0):
         self.date_cursor = date_cursor
@@ -82,24 +61,20 @@ class EventCalendarMonthNavigation:
         self.month = date_cursor + relativedelta(months=self.relativedelta)
         pass
 
-    def __str__(self):
+    def lxml(self):
         atts = {'colspan': 2,
-                'class': 'event-calendar-month-navigation'
-                }
+                'class': 'event-calendar-month-navigation'}
         if self.month.month == self.date_cursor.month:
             atts['class'] += ' active'
         else:
             atts['class'] += ' inactive'
-        th = ET.Element("th", **atts)
-        a = ET.SubElement(th, 'a', href="")
-        a.text = '%s, %s' % (
+
+        text = '%s, %s' % (
             Event.get_monthname(self.month.month, short=True),
             self.month.year)
-        return ET.tostring(th, encoding='unicode')
+        return E.th(E.a(text, href=''),**atts)
 
-
-class EventCalendarMonth:
-    x = XmlOut()
+class CalendarMonth:
     week_headers = Event.get_daynames(True)
 
     def __init__(self, parent, date_cursor):
@@ -118,49 +93,40 @@ class EventCalendarMonth:
         self.weeks = []
         weekstart = self.complete_week_range[0]
         while weekstart <= self.complete_week_range[1]:
-            self.weeks.append(EventCalendarWeek(self, weekstart))
+            self.weeks.append(CalendarWeek(self, weekstart))
             weekstart += relativedelta(days=+7)
             pass
         pass
 
-    def __str__(self):
-        output = ''
-        output += self.x.too('table', {'class': 'event-calendar-month'})
-        output += self.x.too('thead', {'class': 'event-calendar-week'})
-        ##
-        # name and links
-        output += self.x.too('tr')
-        # month
-        output += str(EventCalendarMonthNavigation(self.date_cursor, -1))
-        output += str(EventCalendarMonthNavigation(self.date_cursor))
-        output += str(EventCalendarMonthNavigation(self.date_cursor, +1))
-        output += str(EventCalendarMonthNavigation(self.date_cursor, +2))
-        output += self.x.toc()
-        ##
-        # weekday headers
-        output += self.x.too('tr', {'class': 'event-calendar-week'})
-        output += self.x.toFull('th', {'class': 'kw'}, '')
-        for week in self.week_headers.values():
-            output += self.x.too('th', {'class': 'event-calendar-weekday'})
-            output += self.x.tot(str(week))
-            output += self.x.toc()
-        output += self.x.toc()
-        output += self.x.toc()
-        ##
-        # weeks
-        output += self.x.too('tbody', {'class': 'event-calendar-week'})
-        for week in self.weeks:
-            output += str(week)
-        output += self.x.toc()
-        output += self.x.toc()
-        return output
-
+    def lxml(self):
+        return E.table(
+            E.thead(
+                E.tr(
+                    CalendarMonthNavigation(self.date_cursor, -1).lxml(),
+                    CalendarMonthNavigation(self.date_cursor).lxml(),
+                    CalendarMonthNavigation(self.date_cursor, +1).lxml(),
+                    CalendarMonthNavigation(self.date_cursor, +2).lxml()
+                ),
+                E.tr(
+                    E.th(**{'class': 'kw'}),
+                    *[
+                        E.th(str(week)**{'class': 'event-calendar-weekday'})
+                        for week in self.week_headers.values()
+                    ],
+                    **{'class': 'event-calendar-week'}
+                ),
+                **{'class': 'event-calendar-week'}),
+            E.tbody(
+                *[
+                    week.lxml() for week in self.weeks
+                ],
+                **{'class': 'event-calendar-week'}
+            ),
+            **{'class': 'event-calendar-month'})
     pass
 
 
-class EventCalendarWeek:
-    x = XmlOut()
-
+class CalendarWeek:
     def __init__(self, parent, week_start):
         self.parent = parent
         self.range = []
@@ -169,28 +135,25 @@ class EventCalendarWeek:
         self.days = []
         day = self.range[0]
         while day <= self.range[1]:
-            calendar_day = EventCalendarDay(self, day)
+            calendar_day = CalendarDay(self, day)
             self.days.append(calendar_day)
             day += relativedelta(days=+1)
             pass
         pass
 
-    def __str__(self):
-        output = ''
-        output += self.x.too('tr', {'class': 'event-calendar-week'})
-        output += self.x.toFull('td', {'class': 'kw'}, self.range[0].strftime("%W"))
-        for day in self.days:
-            output += str(day)
-            pass
-        output += self.x.toc()
-        return output
-
+    def lxml(self):
+        return E.tr(
+            E.td(
+                self.range[0].strftime("%W"),
+                **{'class': 'kw'}),
+            *[
+                day.lxml() for day in self.days
+            ],
+            **{'class': 'event-calendar-week'})
     pass
 
 
-class EventCalendarDay:
-    x = XmlOut()
-
+class CalendarDay:
     def __init__(self, parent, date_cursor):
         self.parent = parent
         self.date_cursor = date_cursor
@@ -204,8 +167,7 @@ class EventCalendarDay:
     def in_month(self):
         return self.date_cursor >= self.month.range[0] and self.date_cursor <= self.month.range[1]
 
-    def __str__(self):
-        output = ''
+    def lxml(self):
         atts = {}
         atts['class'] = 'event-calendar-day'
         if self.in_month():
@@ -221,34 +183,12 @@ class EventCalendarDay:
         else:
             atts['class'] += ' noevent'
             pass
-        output += self.x.too('td', atts)
-        if len(events):
-            from event.views import event_list
-            uri = urlresolvers.reverse(event_list,
-                                       args=(),
-                                       kwargs={"year": self.date_cursor.year,
-                                               "month": self.date_cursor.month,
-                                               "day": self.date_cursor.day
-                                               }
-                                       )
-            title = []
+        return E.td(
+            E.a(
+                self.date_cursor.day
+            ),
+            **atts)
 
-            for event in events:
-                title.append(u'%s' % event.name)
-                pass
-            title = u""  # .join(title)
-            atts = {
-                'href': uri,
-                'title': title
-            }
-            output += self.x.toFull('a', atts, self.date_cursor.day)
-            pass
-        else:
-            output += self.x.tot(self.date_cursor.day)
-        output += self.x.toc()
-        return output
-
-    pass
 
 
 if __name__ == '__main__':
