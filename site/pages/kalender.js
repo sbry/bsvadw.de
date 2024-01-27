43790eb4fbc227435b8134b590462caaa50003f4
Justin Salisbury <justin@salisbury.de>
2024-01-27 12:28:20 +0100

    smaller on mobile

--- site/pages/kalender.js
+++ site/pages/kalender.js
@@ -4,8 +4,24 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import iCalendarPlugin from '@fullcalendar/icalendar'
 
 
-
 const Kalender = () => <FullCalendar
+    header={{
+        left: ' prev, next, today, cbFilter',
+        center: 'title',
+        right: 'month, agendaWeek, listMonth'
+    }}
+    mobileHeader={{
+        left: ' prev, today, next',
+        right: 'month, listMonth'
+    }}
+    buttonText={{
+        today: 'Heute',
+        day: 'Tag',
+        week: 'Woche',
+        month: 'Monat'
+    }}
+    height="1000px"
+    hiddenDays={[2, 4, 0]}
     locale='de'
     plugins={[dayGridPlugin, rrulePlugin, iCalendarPlugin]}
     initialView="dayGridMonth"
--- site/pages/lpz-test.js
+++ site/pages/lpz-test.js
@@ -1,8 +1,7 @@
-import Lpz from "../components/lpz";
+// import Lpz from "../components/lpz";
 
 
 const LpzTest = () => <div className={"grid grid-cols-1 md:grid-cols-2 gap-5 "}>
-    <Lpz/>
 </div>
 
 export default LpzTest;
