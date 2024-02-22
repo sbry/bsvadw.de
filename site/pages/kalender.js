5141a296847d0d43142989850caea3165aeb606d
Justin Salisbury <justin@salisbury.de>
2024-02-22 14:34:05 +0100

    reload events on focus!

--- site/pages/kalender.js
+++ site/pages/kalender.js
@@ -2,45 +2,64 @@ import FullCalendar from '@fullcalendar/react'
 import rrulePlugin from '@fullcalendar/rrule'
 import iCalendarPlugin from '@fullcalendar/icalendar'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import React, {useEffect, useRef} from "react";
 
+const Kalender = () => {
+    /*
+     * reload on refocus so people dont call me...
+     * 
+     * 25 years, people still dont reload, actually less than ever
+     */
+    const calendarRef = useRef(null)
 
-const Kalender = () => <FullCalendar
-    moreLinkClassNames={({num, text}) => {
-        var map = {1: "one-third", 2: "two-thirds", 3: "full"}
-        return [ map[num] ];
-    }}
-    moreLinkContent={({num, text}) => {
-        return `${num}`
-    }}
-    views={
-        {
-            dayGridMonthMore: {
-                type: 'dayGridMonth',
-                dayMaxEventRows: 0,
+    useEffect(() => {
+        const onFocus = () => {
+            const calendarApi = calendarRef.current.getApi()
+            calendarApi.refetchEvents()
+        }
+
+        window.addEventListener("focus", onFocus);
+
+        return () => window.removeEventListener("focus", onFocus);
+    });
+
+    return <FullCalendar
+        ref={calendarRef}
+        moreLinkClassNames={({num, text}) => {
+            var map = {1: "one-third", 2: "two-thirds", 3: "full"}
+            return [map[num]];
+        }}
+        moreLinkContent={({num, text}) => {
+            return `${num}`
+        }}
+        views={
+            {
+                dayGridMonthMore: {
+                    type: 'dayGridMonth',
+                    dayMaxEventRows: 0,
+                }
             }
         }
-    }
-    dayMaxEventRows={true}
-    multiMonthMaxColumns={1}
-    buttonText={{
-        today: 'Heute',
-        day: 'Tag',
-        week: 'Woche',
-        month: 'Monat'
-    }}
-    weekNumbers={true}
-    dayHeaders={true}
-    hiddenDays={[2, 4, 0]}
-    locale='de'
-    plugins={[rrulePlugin, iCalendarPlugin, dayGridPlugin]}
-    initialView="dayGridMonthMore"
-    eventSources={[{
-        url: '/bettv.ics',
-        format: 'ics'
-    }, {
-        url: '/google.ics',
-        format: 'ics'
-    }]}
-/>
-
+        dayMaxEventRows={true}
+        buttonText={{
+            today: 'Heute',
+            day: 'Tag',
+            week: 'Woche',
+            month: 'Monat'
+        }}
+        weekNumbers={true}
+        dayHeaders={true}
+        hiddenDays={[2, 4, 0]}
+        locale='de'
+        plugins={[rrulePlugin, iCalendarPlugin, dayGridPlugin]}
+        initialView="dayGridMonthMore"
+        eventSources={[{
+            url: '/bettv.ics',
+            format: 'ics'
+        }, {
+            url: '/google.ics',
+            format: 'ics'
+        }]}
+    />
+}
 export default Kalender;
