c8bad2daad1892971626960b8fccbf75f0c2c8b4
Justin Salisbury <justin@salisbury.de>
2024-03-01 16:37:03 +0100

    added even more context

--- site/pages/lpz-test.js
+++
@@ -1,7 +0,0 @@
-// import Lpz from "../components/lpz";
-
-
-const LpzTest = () => <div className={"grid grid-cols-1 md:grid-cols-2 gap-5 "}>
-</div>
-
-export default LpzTest;
---
+++ site/pages/lpz.js
@@ -0,0 +1,17 @@
+import Lpz from "../components/lpz";
+
+
+const LpzTest = () =>
+
+
+    <div>
+
+        <Lpz/>
+
+        {/*<div className={"wt-5"}>*/}
+        {/*    Danke jedenfalls an <a target="_blank" href={"https://xolay.com/de/ttr-rechner"}>XOLAY - Die*/}
+        {/*    Tischtennisschule</a> und <a target="_blank" href={"https://tt-test.de/ttr-rechner/"}>TT-Test</a>*/}
+        {/*</div>*/}
+    </div>
+
+export default LpzTest;
