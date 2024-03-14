6444bed07b1bbdb0da553fc6dbead5e05cc29cfb
Justin Salisbury <justin@salisbury.de>
2024-03-14 17:51:39 +0100

    now it works and we dont have to use filezilla anymore!

--- sync.py
+++ sync.py
@@ -11,6 +11,9 @@ DEBUG=False
 
 if DEBUG:
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+else:
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
 logger = logging.getLogger()
 
 
@@ -19,6 +22,7 @@ class FTP_TLS_BSVADW(ftplib.FTP_TLS):
         super().__init__()
         if DEBUG:
             self.set_debuglevel(1)
+            pass
         self.connect("***REMOVED***", port=***REMOVED***)
         self.login(user="***REMOVED***", passwd="***REMOVED***")
         self.prot_p()
@@ -31,6 +35,7 @@ class FTP_TLS_BSVADW(ftplib.FTP_TLS):
                                             server_hostname=self.host,
                                             session=self.sock.session)  # this is the fix
         return conn, size
+    pass
 
 
 def usage():
@@ -38,23 +43,9 @@ def usage():
     print(f"{sys.argv[0]} push|pull|archive|pull_ics|push_ics")
     sys.exit()
 
-
-def archive_home():
-    local_fs = fs.open_fs('osfs://')
-    local_fs.makedir('arch', recreate=True)
-    if local_fs.exists("home"):
-        local_fs.movedir('home', f'arch/{datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}', create=True)
-
-
-if __name__ == '__main__':
-    mode = ""
-    try:
-        mode = sys.argv[1]
-    except IndexError:
-        usage()
-    if mode == "push":
-        homePath = pathlib.Path("home/html")
-        remoteConnection = FTP_TLS_BSVADW()
+def push():
+    homePath = pathlib.Path("home/html")
+    with FTP_TLS_BSVADW() as remoteConnection:
         for homeFilePath in homePath.rglob('*'):
             relativePath = homeFilePath.relative_to("home/html")
             if homeFilePath.is_dir():
@@ -62,35 +53,49 @@ if __name__ == '__main__':
                     remoteConnection.mkd('./' + str(relativePath))
                 except ftplib.error_perm:
                     logger.debug("Directory already exists %s", relativePath)
+                    pass
+                remoteConnection.sendcmd(f"SITE CHMOD 755 {relativePath}")
             else:
                 with homeFilePath.open("rb") as fh:
                     remoteConnection.storbinary(f"STOR {relativePath}", fh)
-                    remoteConnection.sendcmd(f"SITE CHMOD 755 {relativePath}")
+                    remoteConnection.sendcmd(f"SITE CHMOD 644 {relativePath}")
                     logger.debug("File Uploaded %s", relativePath)
-    elif mode == "archive":
-        archive_home()
-    elif mode == "pull_ics":
-        bsvadw = {
-            "bettv": "https://bettv.tischtennislive.de/export/Tischtennis/iCal.aspx?Typ=Verein&ID=***REMOVED***&Runde=2&Hallenplan=True",
-            "google": "https://calendar.google.com/calendar/ical/***REMOVED***%40group.calendar.google.com/private-***REMOVED***/basic.ics"
-        }
-        for filename, url in bsvadw.items():
-            with (open(f"site/public/{filename}.ics", "wb") as f1,
-                  open(f"home/html/{filename}.ics", "wb") as f2):
-                contents = urllib.request.urlopen(url).read()
-                f1.write(contents)
-                f2.write(contents)
+                    pass
                 pass
             pass
         pass
-    elif mode == 'push_ics':
-        homePath = pathlib.Path("home/html")
-        remoteConnection = FTP_TLS_BSVADW()
+    pass
+
+def pull_ics():
+    bsvadw = {
+        "bettv": "https://bettv.tischtennislive.de/export/Tischtennis/iCal.aspx?Typ=Verein&ID=***REMOVED***&Runde=2&Hallenplan=True",
+        "google": "https://calendar.google.com/calendar/ical/***REMOVED***%40group.calendar.google.com/private-***REMOVED***/basic.ics"
+    }
+    for filename, url in bsvadw.items():
+        with (open(f"site/public/{filename}.ics", "wb") as f1,
+              open(f"home/html/{filename}.ics", "wb") as f2):
+            contents = urllib.request.urlopen(url).read()
+            f1.write(contents)
+            f2.write(contents)
+            pass
+        pass
+    pass
+
+def push_ics():
+    homePath = pathlib.Path("home/html")
+    with FTP_TLS_BSVADW() as remoteConnection:
         for basename in ['google.ics', "bettv.ics"]:
             filePath = homePath / basename
             with filePath.open("rb") as file:
                 remoteConnection.storbinary(f"STOR {basename}", file)
-            remoteConnection.sendcmd(f"SITE CHMOD 644 {basename}")
-        remoteConnection.quit()
-    else:
-        usage()
+                remoteConnection.sendcmd(f"SITE CHMOD 644 {basename}")
+                pass
+            pass
+        pass
+    pass
+
+
+
+
+if __name__ == '__main__':
+    globals().get(sys.argv[1], "usage")()
