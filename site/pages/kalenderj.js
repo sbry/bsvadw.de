eeec32eb93231f0a94ef96f3a505020093ee143b
Justin Salisbury <justin@salisbury.de>
2024-09-27 12:04:07 +0200

    latest calendar

--- site/pages/kalenderj.js
+++ site/pages/kalenderj.js
@@ -23,44 +23,47 @@ const Kalenderj = () => {
         return () => window.removeEventListener("focus", onFocus);
     });
 
-    return <div><h1 style={{position: "absolute", right: 0, backgroundColor: '#0000ff', color:'#ffffff'}}>Jugend</h1><FullCalendar
-        ref={calendarRef}
-        moreLinkClassNames={({num, text}) => {
-            var map = {1: "one-third", 2: "two-thirds", 3: "full"}
-            return [map[num]];
-        }}
-        moreLinkContent={({num, text}) => {
-            return `${num}`
-        }}
-        views={
-            {
-                dayGridMonthMore: {
-                    type: 'dayGridMonth',
-                    dayMaxEventRows: 2,
+    return <div><h1 style={{position: "absolute", right: 0, backgroundColor: '#0000ff', color: '#ffffff'}}>Jugend</h1>
+        <FullCalendar
+            ref={calendarRef}
+            moreLinkClassNames={({num, text}) => {
+                var map = {1: "one-third", 2: "two-thirds", 3: "full"}
+                return [map[num]];
+            }}
+            moreLinkContent={({num, text}) => {
+                return `${num}`
+            }}
+            views={
+                {
+                    dayGridMonthMore: {
+                        type: 'dayGridMonth',
+                        dayMaxEventRows: 2,
+                    }
                 }
             }
-        }
-        dayMaxEventRows={true}
-        buttonText={{
-            today: 'Heute',
-            day: 'Tag',
-            week: 'Woche',
-            month: 'Monat'
-        }}
-        weekNumbers={true}
-        dayHeaders={true}
-        hiddenDays={[3, 4, 6, 0]}
-        locale='de'
-        plugins={[rrulePlugin, iCalendarPlugin, dayGridPlugin]}
-        initialView="dayGridMonthMore"
-        eventSources={[
-            {
-                url: '/termine-jugend.ics',
-                format: 'ics',
-                color: 'orange',   // an option!
+            dayMaxEventRows={true}
+            buttonText={{
+                today: 'Heute',
+                day: 'Tag',
+                week: 'Woche',
+                month: 'Monat'
+            }}
+            weekNumbers={true}
+            dayHeaders={true}
+            hiddenDays={[
+                // 3, 4, 6, 0
+            ]}
+            locale='de'
+            plugins={[rrulePlugin, iCalendarPlugin, dayGridPlugin]}
+            initialView="dayGridMonthMore"
+            eventSources={[
+                {
+                    url: '/termine-jugend.ics',
+                    format: 'ics',
+                    color: 'orange',   // an option!
 
-            }
-        ]}
-    /></div>
+                }
+            ]}
+        /></div>
 }
 export default Kalenderj;
