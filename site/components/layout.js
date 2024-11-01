5cefa36d96510ed1730064f34f0cae8e85516b71
Justin Salisbury <justin@salisbury.de>
2024-11-01 12:39:50 +0100

    favicons

--- site/components/layout.js
+++ site/components/layout.js
@@ -8,6 +8,13 @@ const Layout = ({children}) => {
             <Head>
                 <title>BSV AdW e.V. Tischtennis</title>
                 <link rel="icon" href="/favicon.ico"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/logo/adw-symbol.png"/>
+                <link rel="apple-touch-icon" sizes="72x72" href="/images/logo/adw-symbol-72.png"/>
+                <link rel="apple-touch-icon" sizes="76x76" href="/images/logo/adw-symbol-76.png"/>
+                <link rel="apple-touch-icon" sizes="114x114" href="/images/logo/adw-symbol-114.png"/>
+                <link rel="apple-touch-icon" sizes="120x120" href="/images/logo/adw-symbol-120.png"/>
+                <link rel="apple-touch-icon" sizes="144x144" href="/images/logo/adw-symbol-144.png"/>
+                <link rel="apple-touch-icon" sizes="152x152" href="/images/logo/adw-symbol-152.png"/>
             </Head>
             <Header/>
             <main>{children}</main>
