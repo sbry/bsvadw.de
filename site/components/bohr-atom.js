5d6cb5c68d8b88f8efb9fda9a9052ecd27c34310
Justin Salisbury <technik@perlentaucher.de>
2025-06-03 12:02:24 +0200

    cleanup the big success from this morning!

--- site/components/bohr-atom.js
+++ site/components/bohr-atom.js
@@ -1,41 +1,27 @@
 import React from 'react';
 
-
-const Electron = ({
-                      dur = 2,
-                      position = 1,
-                      mpath_href, transform
-                  }) => {
-    return (
-        <g fill="none" stroke="bbb" strokeWidth="6px" transform={transform}>
-
-            <circle
-                fill="#d3181a"
-                stroke="none" r={8}>
-
-                <animateMotion dur={`${dur}s`} begin={`${dur - position}s`}
-                               repeatDur={"indefinite"}
-                               repeatCount="indefinite">
-                    <mpath xlinkHref={mpath_href}/>
-                </animateMotion>
-
-            </circle>
-        </g>
-    )
-}
-
-const BohrAtom = ({
-                      ...props
-                  }) => {
+/**
+ *  we learned a lot here:
+ * https://stackoverflow.com/questions/55632058/set-origin-and-rotation-on-animatemotions-path
+ */
+const BohrAtom = ({...props}) => {
     const width = props.width || 100;
     const height = props.height || 100;
+    const counter = props.counter || 2;
+    const center_x = width / 2;
+    const center_y = height / 2;
     const transforms = [
-        `rotate(45, ${width / 2}, ${width / 2})`,
-        `rotate(-45, ${width / 2}, ${width / 2})`
+        `rotate(45, ${center_x}, ${center_y})`,
+        `rotate(-45, ${center_x}, ${center_y})`
     ]
+    /*
+     * three electrons per ellipse
+     */
     const electrons = [
         1, 2, 3
     ]
+    // @todo not quite clear about the radii of the ellipse
+    // @todo would be nice to have a soft rotation of the whole thing
     return <svg viewBox={`"0 0 ${width} ${height}`}
                 preserveAspectRatio="xMidYMid meet" role="img"
                 width={width} height={height}
@@ -44,10 +30,13 @@ const BohrAtom = ({
         <defs>
             <ellipse id="orbit"
                      fill="none" strokeWidth={width / 50} stroke="#fff"
-                     cx={width / 2} cy={height / 2}
-                     rx={width / 5} ry={width / 2}/>
+                     cx={center_x} cy={center_y}
+
+                     rx={width / 5}
+                     ry={width / 2}
+            />
         </defs>
-        
+
         <circle id="atom" cx={width / 2} cy={height / 2} r={width / 2} fill="#20782d"/>
         <circle id="nucleus" fill="#fff" cx={width / 2} cy={height / 2} r={width / 10}/>
 
