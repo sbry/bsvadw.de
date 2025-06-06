9bceda1f657265140e197014b5a058c65506e6de
Justin Salisbury <justin@salisbury.de>
2025-06-06 12:11:04 +0200

    does not work but why?

--- site/pages/atom.js
+++
@@ -1,9 +0,0 @@
-import BohrAtom from "../components/bohr-atom";
-
-const atom = () => {
-    return <div className={'max-w-screen'}>
-        <BohrAtom width={800} height={800}/>
-    </div>;
-}
-
-export default atom;
---
+++ site/pages/logo.js
@@ -0,0 +1,6 @@
+import BohrAtom from "../components/bohr-atom";
+
+const Logo = () => <BohrAtom width={800} height={800}/>
+
+
+export default Logo;
