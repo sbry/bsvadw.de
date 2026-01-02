2ed708ad46db9873c3beb00c5cbaf39af31825a0
Justin Salisbury <justin@www.app1.de>
2026-01-02 11:31:27 +0000

    use venv

--- cron_push_ics.sh
+++ cron_push_ics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python=/home/justin/workspace/pyenv/versions/3.12.1/bin/python
+. $(dirname $0)/python-venv/bin/activate
 if [ -n "$TERM" -a "$TERM" == "dumb" ]; then
 pushd() {
 command pushd "$@" >/dev/null
@@ -14,8 +14,8 @@ pushd $(dirname $0);
 	echo "Begin $(date)"
 	time_before=$(date +%s)
 	. .env
-	$python ./sync.py pull_ics; 
-	$python ./sync.py push_ics;
+	python ./sync.py pull_ics; 
+	python ./sync.py push_ics;
 	time_after=$(date +%s)
 	echo "$(($time_after - $time_before))s"
 	echo
